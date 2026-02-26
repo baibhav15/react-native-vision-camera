6d0d31cb640866ffd4171f31fb3e1336a394cac0
Baibhav Ojha <baibhav.ojha@thenovaweb.com>
2026-02-26 17:35:31 +0400

    Resolve camera fetching by adding a delayed function to access camera devices after initialization, this is to avoid deviceChanges webhook getting trigerred before the JS layer is initialized while still being able to access the camera list by updating the constants

--- src/CameraDevices.ts
+++ src/CameraDevices.ts
@@ -1,27 +1,54 @@
-import { NativeModules, NativeEventEmitter } from 'react-native'
-import type { CameraDevice } from './types/CameraDevice'
+import { NativeModules, NativeEventEmitter, Platform } from "react-native";
+import type { CameraDevice } from "./types/CameraDevice";
 
 const CameraDevicesManager = NativeModules.CameraDevices as {
   getConstants: () => {
-    availableCameraDevices: CameraDevice[]
-    userPreferredCameraDevice: CameraDevice | undefined
+    availableCameraDevices: CameraDevice[];
+    userPreferredCameraDevice: CameraDevice | undefined;
+  };
+  getAvailableDeviceManually: () => Promise<CameraDevice[]>;
+};
+
+const isAndroid = Platform.OS === "android";
+
+const constants = CameraDevicesManager.getConstants();
+let devices = constants.availableCameraDevices;
+
+const DEVICES_CHANGED_NAME = "CameraDevicesChanged";
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const eventEmitter = new NativeEventEmitter(CameraDevicesManager as any);
+eventEmitter.addListener(DEVICES_CHANGED_NAME, (newDevices: CameraDevice[]) => {
+  devices = newDevices;
+});
+
+// On Android, sometimes the devices are not ready when the module is initialized.
+// So we try to fetch them again after a delay if none are available.
+if (isAndroid) {
+  if ((devices?.length || 0) === 0) {
+    setTimeout(() => {
+      CameraDevicesManager.getAvailableDeviceManually().then((newDevices) => {
+        devices = newDevices;
+      });
+    }, 5000);
   }
 }
-
-const constants = CameraDevicesManager.getConstants()
-let devices = constants.availableCameraDevices
-
-const DEVICES_CHANGED_NAME = 'CameraDevicesChanged'
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const eventEmitter = new NativeEventEmitter(CameraDevicesManager as any)
-eventEmitter.addListener(DEVICES_CHANGED_NAME, (newDevices: CameraDevice[]) => {
-  devices = newDevices
-})
 
 export const CameraDevices = {
   userPreferredCameraDevice: constants.userPreferredCameraDevice,
   getAvailableCameraDevices: () => devices,
-  addCameraDevicesChangedListener: (callback: (newDevices: CameraDevice[]) => void) => {
-    return eventEmitter.addListener(DEVICES_CHANGED_NAME, callback)
+  getAvailableCameraDevicesManually: async () => {
+    if (isAndroid) {
+      const newDevices =
+        await CameraDevicesManager.getAvailableDeviceManually();
+      if ((newDevices?.length || 0) > 0) devices = newDevices;
+
+      return newDevices;
+    }
+    return Promise.resolve([]);
   },
-}
+  addCameraDevicesChangedListener: (
+    callback: (newDevices: CameraDevice[]) => void,
+  ) => {
+    return eventEmitter.addListener(DEVICES_CHANGED_NAME, callback);
+  },
+};
--- src/hooks/useCameraDevices.ts
+++ src/hooks/useCameraDevices.ts
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
-import type { CameraDevice } from '../types/CameraDevice'
-import { CameraDevices } from '../CameraDevices'
+import { useEffect, useRef, useState } from "react";
+import type { CameraDevice } from "../types/CameraDevice";
+import { CameraDevices } from "../CameraDevices";
 
 /**
  * Get all available Camera Devices this phone has.
@@ -10,14 +10,30 @@ import { CameraDevices } from '../CameraDevices'
  * so the result of this function might update over time.
  */
 export function useCameraDevices(): CameraDevice[] {
-  const [devices, setDevices] = useState(() => CameraDevices.getAvailableCameraDevices())
+  const [devices, setDevices] = useState(() =>
+    CameraDevices.getAvailableCameraDevices(),
+  );
+  const numberOfDevicesRef = useRef(devices.length);
 
   useEffect(() => {
-    const listener = CameraDevices.addCameraDevicesChangedListener((newDevices) => {
-      setDevices(newDevices)
-    })
-    return () => listener.remove()
-  }, [])
+    let isMounted = true;
+    const listener = CameraDevices.addCameraDevicesChangedListener(
+      (newDevices) => {
+        setDevices(newDevices);
+      },
+    );
+    // Only update if we got new devices and the component is still mounted
+    // This happens with Android only
+    if (numberOfDevicesRef.current === 0) {
+      CameraDevices.getAvailableCameraDevicesManually().then((newDevices) => {
+        if (isMounted && newDevices.length > 0) setDevices(newDevices);
+      });
+    }
+    return () => {
+      isMounted = false;
+      listener.remove();
+    };
+  }, []);
 
-  return devices
+  return devices;
 }
